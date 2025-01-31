5a6e00b6d0fd1ba8fcce524697f4f52a6899f778
Jefferson Quesado <jefferson.neto@trustly.com>
2025-01-31 10:53:14 -0300

    Reproducing the error

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 Reproducing issue https://github.com/oracle/graaljs/issues/886 as a standalone maven project.
+
+## How to reproduce
+
+To run with graalvm-24, version that the bug was reported:
+
+```bash
+./mvnw compile exec:java -P graalvm-24
+```
+
+It will yield the error:
+
+```none
+Exception in thread "main" TypeError: k.equals is not a function
+	at <js> :=>(Unnamed:6:154-169)
+	at com.oracle.truffle.polyglot.PolyglotFunctionProxyHandler.invoke(PolyglotFunctionProxyHandler.java:151)
+```
+
+To run the previous known version with no error (graalvm-20):
+
+```bash
+./mvnw compile exec:java -P graalvm-20
+```
+
+It will print:
+
+```none
+Optional[true]
+```
--- src/main/java/com/jeffque/App.java
+++ src/main/java/com/jeffque/App.java
@@ -1,10 +1,29 @@
 package com.jeffque;
 
-/**
- * Hello world!
- */
+import org.graalvm.polyglot.Context;
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+         final var context = Context.newBuilder()
+                .allowExperimentalOptions(true)
+                .option("js.nashorn-compat", "true")
+                .option("js.ecmascript-version", "2020")
+                .allowAllAccess(true)
+                .build();
+
+        final var script =
+        """
+        var namesList = java.util.List.of("test","test1");
+        
+        namesList.stream()
+        .findFirst()
+        .map(s => java.util.stream.Stream.of(s.split(' '))
+            .anyMatch(k => k.equals('test'))
+        )
+        """;
+        final var contextEval = context.eval("js", script);
+
+        System.out.println(contextEval.toString());
     }
 }
